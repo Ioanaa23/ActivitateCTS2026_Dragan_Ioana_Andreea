5a23b8a3d3eb5fa4892650466fdee6229fc400a5
Ioanaa23 <drg.deea23@gmail.com>
2026-02-26 17:52:15 +0200

    creare seminar 2

---
+++ Seminar2/src/ro/ase/cts/main/Program.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.main;
+
+import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.classes.Utils;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = Utils.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
